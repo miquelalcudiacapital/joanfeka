ea97da446453454faee16b79b49fe3d3582952a9
Luffy <3399349@gmail.com>
2020-01-21 18:48:06 +0100

    Afegit algo a Report.java

--- Report.java
+++ Report.java
@@ -2,6 +2,7 @@ public class Report {
 	public static void main(String[] args) {
         System.out.println("Hola món, què tal?");
 		System.out.println("Estic bé");
-		System.out.println("Estoy Doramion");
+        System.out.println("Estoy Doramion");
+        System.out.println("Prova");
 	}
 }
