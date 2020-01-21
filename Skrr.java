8ee88e8345ca2db1bee2b4470cb320af9615d03f
Miquel <miquelpascual01@gmail.com>
2020-01-21 18:34:52 +0100

    Añadido Goteo

--- Skrr.java
+++ Skrr.java
@@ -1,5 +1,6 @@
 public class Skrr {
     public static void main(String[] args) {
         System.out.println("SKERIIIIIIIIIIIIIIIIIIIIT");
+        System.out.println("Me puse las gucci con un short de Nike, buzo y cadena ya estoy que goteo");
     }
 }
