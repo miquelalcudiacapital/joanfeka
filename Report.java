372ed22df4848a8fe85d26ce559c3b1f94288193
Miquel <miquelpascual01@gmail.com>
2020-01-21 18:38:13 +0100

    Añadido Doramion

--- Report.java
+++ Report.java
@@ -1,6 +1,7 @@
 public class Report {
 	public static void main(String[] args) {
         System.out.println("Hola món, què tal?");
-        System.out.println("Estic bé");
+		System.out.println("Estic bé");
+		System.out.println("Estoy Doramion");
 	}
 }
